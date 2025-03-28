166e1503a5dc1b55e75ebf5d80364cd8e046d961
Tim Halmshaw <timhalmshaw@gmail.com>
2025-03-28 16:02:26 +0000

    Fix project consistency: Add comprehensive docstrings to core.clj, update README, and consolidate GitHub workflows

--- specifications/dsl/functional-clj/finapp-dsl/README.md
+++ specifications/dsl/functional-clj/finapp-dsl/README.md
@@ -1,36 +1,114 @@
-# finapp-dsl
+# FinApp DSL
 
-FIXME: description
+A functional domain-specific language (DSL) for financial applications, focusing on business rules, customer journeys, and multi-region support.
+
+## Overview
+
+FinApp DSL is a Clojure-based implementation of a functional programming model that powers test execution and business logic for financial applications. It enables precise definition of business rules, customer journeys, and calculations with support for:
+
+- Multi-region behavior (UK, Hong Kong, etc.)
+- Customer segmentation
+- Business rule evaluation
+- Financial calculations
+- Consistent behavior across platforms
 
 ## Installation
 
-Download from http://example.com/FIXME.
+Add the following dependency to your `project.clj`:
+
+```clojure
+[finapp-dsl "0.1.0-SNAPSHOT"]
+```
+
+Or install from source:
+
+```bash
+git clone https://github.com/AppAthanor/finapp-dsl-architecture.git
+cd finapp-dsl-architecture/specifications/dsl/functional-clj/finapp-dsl
+lein install
+```
+
+## Documentation
+
+The API documentation is available at:
+- [Online documentation](https://appanthanor.github.io/finapp-dsl-architecture/)
+- Local: Generate with `make docs` and view at `doc/api/index.html`
+
+For more details on the documentation system, see [Documentation Automation](doc/AUTOMATION.md).
 
 ## Usage
 
-FIXME: explanation
+```clojure
+(require '[finapp-dsl.core :as dsl])
 
-    $ java -jar finapp-dsl-0.1.0-standalone.jar [args]
+;; Create an evaluation environment with business context
+(def env (dsl/create-global-environment))
 
-## Options
+;; Define a simple business rule
+(def eligibility-rule 
+  (dsl/make-business-rule
+    "BR001"
+    (dsl/make-lambda ["customer" "region"] 
+      (dsl/make-application >= [(dsl/make-application :balance [(dsl/make-variable "customer")]) 1000]))
+    (dsl/make-lambda ["customer" "region"]
+      (dsl/make-quoted "Customer is eligible"))))
 
-FIXME: listing of options this app accepts.
+;; Evaluate expressions against an environment
+(def customer {:id "C123" :balance 1500 :segment "Basic"})
+(dsl/evaluate 
+  (dsl/make-application eligibility-rule [customer dsl/uk-region])
+  env)
+;; => "Customer is eligible"
+```
 
 ## Examples
 
-...
+### Business Rule Definition
 
-### Bugs
+```clojure
+(def topup-amount-limits-rule
+  (dsl/make-business-rule
+    "BR002"
+    ;; Condition expression that checks if amount is within segment limits
+    (dsl/make-lambda ["customer" "region" "amount"] 
+      (dsl/make-application <= [(dsl/make-variable "amount") 
+                               (dsl/make-application :max-amount 
+                                 [(dsl/make-variable "customer") 
+                                  (dsl/make-variable "region")])])),
+    ;; Action expression that validates or adjusts the amount
+    (dsl/make-lambda ["customer" "region" "amount"] 
+      (dsl/make-if 
+        (dsl/make-application <= [(dsl/make-variable "amount") 0])
+        (dsl/make-quoted "Amount must be positive")
+        (dsl/make-variable "amount")))))
+```
 
-...
+### Customer Segmentation
 
-### Any Other Sections
-### That You Think
-### Might be Useful
+```clojure
+(def wealth-segment-benefits
+  (dsl/make-lambda ["region"]
+    (dsl/make-if
+      (dsl/make-application = [(dsl/make-variable "region") dsl/uk-region])
+      (dsl/make-quoted "Premier rate guarantee and priority service")
+      (dsl/make-quoted "Jade member priority processing and rate discount"))))
+```
+
+## Development
+
+Run tests:
+```bash
+lein test
+```
+
+Format code:
+```bash
+lein cljfmt fix
+```
 
 ## License
 
-Copyright © 2025 FIXME
+Copyright © 2025 AppAthanor
 
 This program and the accompanying materials are made available under the
 terms of the Eclipse Public License 2.0 which is available at
--- specifications/dsl/functional-clj/finapp-dsl/src/finapp_dsl/core.clj
+++ specifications/dsl/functional-clj/finapp-dsl/src/finapp_dsl/core.clj
@@ -1,93 +1,272 @@
 (ns finapp-dsl.core
+  "Core namespace for Financial Application DSL. Provides functions to create, manipulate,
+  and evaluate financial expressions and rules within a domain-specific language for
+  financial applications."
   (:gen-class))
 
 ;; Expression types
-(defn make-variable [name]
+(defn make-variable
+  "Creates a variable expression with the given name.
+   
+   Parameters:
+     name - A string or symbol representing the variable name
+   
+   Returns:
+     A map representing a variable expression with :type and :name keys"
+  [name]
   {:type :variable :name name})
 
-(defn make-application [operator operands]
+(defn make-application
+  "Creates an application expression representing a function call.
+   
+   Parameters:
+     operator - The function to apply
+     operands - A sequence of arguments to pass to the function
+   
+   Returns:
+     A map representing an application expression with :type, :operator, and :operands keys"
+  [operator operands]
   {:type :application :operator operator :operands operands})
 
-(defn make-lambda [parameters body]
+(defn make-lambda
+  "Creates a lambda expression representing an anonymous function.
+   
+   Parameters:
+     parameters - A sequence of parameter names
+     body - The body expression to evaluate when the function is called
+   
+   Returns:
+     A map representing a lambda expression with :type, :parameters, and :body keys"
+  [parameters body]
   {:type :lambda :parameters parameters :body body})
 
-(defn make-if [predicate consequent alternative]
+(defn make-if
+  "Creates a conditional expression.
+   
+   Parameters:
+     predicate - The condition to evaluate
+     consequent - The expression to evaluate if predicate is true
+     alternative - The expression to evaluate if predicate is false
+   
+   Returns:
+     A map representing a conditional expression with :type, :predicate, :consequent, and :alternative keys"
+  [predicate consequent alternative]
   {:type :if :predicate predicate :consequent consequent :alternative alternative})
 
-(defn make-assignment [variable value]
+(defn make-assignment
+  "Creates an assignment expression to update a variable's value.
+   
+   Parameters:
+     variable - The variable to assign to
+     value - The value to assign
+   
+   Returns:
+     A map representing an assignment expression with :type, :variable, and :value keys"
+  [variable value]
   {:type :assignment :variable variable :value value})
 
-(defn make-sequence [expressions]
+(defn make-sequence
+  "Creates a sequence expression to evaluate multiple expressions in order.
+   
+   Parameters:
+     expressions - A sequence of expressions to evaluate
+   
+   Returns:
+     A map representing a sequence expression with :type and :expressions keys"
+  [expressions]
   {:type :sequence :expressions expressions})
 
-(defn make-quoted [text]
+(defn make-quoted
+  "Creates a quoted expression that returns its text without evaluation.
+   
+   Parameters:
+     text - The text to quote
+   
+   Returns:
+     A map representing a quoted expression with :type and :text keys"
+  [text]
   {:type :quoted :text text})
 
 ;; Environment operations
-(defn extend-environment [vars vals base-env]
+(defn extend-environment
+  "Extends an environment with new variable bindings.
+   
+   Parameters:
+     vars - A sequence of variable names
+     vals - A sequence of values to bind to the variables
+     base-env - The environment to extend
+   
+   Returns:
+     A new environment containing the new bindings with the base environment as parent"
+  [vars vals base-env]
   (assoc base-env :frame (zipmap vars vals) :base base-env))
 
-(defn lookup-variable-value [var env]
+(defn lookup-variable-value
+  "Looks up the value of a variable in an environment.
+   
+   Parameters:
+     var - The variable to look up
+     env - The environment to search in
+   
+   Returns:
+     The value bound to the variable
+   
+   Throws:
+     Exception if the variable is unbound in the environment"
+  [var env]
   (let [name (:name var)]
     (cond
       (nil? env) (throw (Exception. (str "Unbound variable: " name)))
       (contains? (:frame env) name) (get-in env [:frame name])
       :else (lookup-variable-value var (:base env)))))
 
-(defn define-variable! [var val env]
+(defn define-variable!
+  "Defines a variable in the current environment frame.
+   
+   Parameters:
+     var - The variable to define
+     val - The value to bind to the variable
+     env - The environment to define the variable in
+   
+   Returns:
+     The updated environment with the new binding"
+  [var val env]
   (let [name (:name var)]
     (assoc-in env [:frame name] val)))
 
-(defn set-variable-value! [var val env]
+(defn set-variable-value!
+  "Sets the value of an existing variable in an environment.
+   
+   Parameters:
+     var - The variable to set
+     val - The new value for the variable
+     env - The environment to update
+   
+   Returns:
+     The updated environment with the new value
+   
+   Throws:
+     Exception if the variable is unbound in the environment"
+  [var val env]
   (let [name (:name var)]
     (cond
       (nil? env) (throw (Exception. (str "Unbound variable: " name)))
       (contains? (:frame env) name) (assoc-in env [:frame name] val)
       :else (set-variable-value! var val (:base env)))))
 
-(defn create-global-environment []
+(defn create-global-environment
+  "Creates a new global environment with an empty frame.
+   
+   Returns:
+     A new environment with an empty frame and no parent environment"
+  []
   {:frame {} :base nil})
 
 ;; Evaluation
 (declare evaluate)
 
-(defn eval-sequence [expressions env]
+(defn eval-sequence
+  "Evaluates a sequence of expressions in order, returning the result of the last expression.
+   
+   Parameters:
+     expressions - The sequence of expressions to evaluate
+     env - The environment for evaluation
+   
+   Returns:
+     The result of evaluating the last expression in the sequence"
+  [expressions env]
   (if (empty? (rest expressions))
     (evaluate (first expressions) env)
     (do
       (evaluate (first expressions) env)
       (eval-sequence (rest expressions) env))))
 
-(defn eval-assignment [exp env]
+(defn eval-assignment
+  "Evaluates an assignment expression by updating a variable in the environment.
+   
+   Parameters:
+     exp - The assignment expression
+     env - The environment for evaluation
+   
+   Returns:
+     The value that was assigned"
+  [exp env]
   (let [var (:variable exp)
         val (evaluate (:value exp) env)]
     (set-variable-value! var val env)
     val))
 
-(defn eval-definition [exp env]
+(defn eval-definition
+  "Evaluates a definition expression by adding a new binding to the environment.
+   
+   Parameters:
+     exp - The definition expression
+     env - The environment for evaluation
+   
+   Returns:
+     The value that was defined"
+  [exp env]
   (let [var (:variable exp)
         val (evaluate (:value exp) env)]
     (define-variable! var val env)
     val))
 
-(defn eval-if [exp env]
+(defn eval-if
+  "Evaluates a conditional expression by first evaluating the predicate,
+   then evaluating either the consequent or alternative based on the result.
+   
+   Parameters:
+     exp - The conditional expression
+     env - The environment for evaluation
+   
+   Returns:
+     The result of evaluating either the consequent or alternative"
+  [exp env]
   (let [predicate-value (evaluate (:predicate exp) env)]
     (if predicate-value
       (evaluate (:consequent exp) env)
       (evaluate (:alternative exp) env))))
 
-(defn eval-lambda [exp env]
+(defn eval-lambda
+  "Evaluates a lambda expression by creating a function that captures the environment.
+   
+   Parameters:
+     exp - The lambda expression
+     env - The environment at definition time
+   
+   Returns:
+     A function that, when called, evaluates the body in an extended environment"
+  [exp env]
   (fn [& args]
     (let [params (:parameters exp)
           new-env (extend-environment params args env)]
       (evaluate (:body exp) new-env))))
 
-(defn eval-application [exp env]
+(defn eval-application
+  "Evaluates a function application by evaluating the operator and operands,
+   then applying the operator function to the operand values.
+   
+   Parameters:
+     exp - The application expression
+     env - The environment for evaluation
+   
+   Returns:
+     The result of applying the operator to the operands"
+  [exp env]
   (let [operator (evaluate (:operator exp) env)
         operands (map #(evaluate % env) (:operands exp))]
     (apply operator operands)))
 
-(defn evaluate [exp env]
+(defn evaluate
+  "Evaluates an expression in a given environment.
+   
+   Parameters:
+     exp - The expression to evaluate
+     env - The environment for evaluation
+   
+   Returns:
+     The result of evaluating the expression"
+  [exp env]
   (cond
     (map? exp) (case (:type exp)
                  :variable (lookup-variable-value exp env)
@@ -101,15 +280,32 @@
     :else exp))
 
 ;; Business domain
-(def uk-region {:code "UK" :name "United Kingdom"})
-(def hk-region {:code "HK" :name "Hong Kong"})
+(def uk-region 
+  "Region definition for the United Kingdom"
+  {:code "UK" :name "United Kingdom"})
 
-(def basic-segment {:code "Basic" :name "Basic Customer"})
-(def wealth-segment {:code "Wealth" :name "Wealth Management Customer"})
+(def hk-region 
+  "Region definition for Hong Kong"
+  {:code "HK" :name "Hong Kong"})
+
+(def basic-segment 
+  "Customer segment definition for basic customers"
+  {:code "Basic" :name "Basic Customer"})
+
+(def wealth-segment 
+  "Customer segment definition for wealth management customers"
+  {:code "Wealth" :name "Wealth Management Customer"})
 
 ;; Main function
 (defn -main
-  "Financial DSL entry point"
+  "Financial DSL entry point. Initializes the DSL environment and
+   prepares it for evaluating financial expressions.
+   
+   Parameters:
+     args - Command line arguments (not used)
+   
+   Returns:
+     nil"
   [& args]
   (println "Financial DSL running...")
   (let [env (create-global-environment)]
